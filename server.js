e6112c38354f0881cb6e8f944b48ed8c0127ee25
alexdoesnotexist1 desktop <alexanderharmonic@gmail.com>
2024-12-20 18:40:03 -0500

    edited server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
-// const Product = require('./models/productModel')
+const cors = require('cors')
 const Task = require('./models/taskModel')
 const app = express()
+
+app.use(cors());
 
 //middleware
 app.use(express.json())
@@ -17,70 +19,6 @@ app.get('/', (req, res) => {
 app.get('/blog', (req, res) => {
     res.send('blogbltgoglgosyasssqueenoglgogg')
 })
-
-// app.get('/products', async(req, res) => {
-//     try {
-//         const products = await Product.find({});
-//         res.status(200).json(products);
-//     } catch (error) {
-//         res. status(500).json({message: error.message})
-//     }
-// })
-
-// app.get('/products/:id', async(req, res) => {
-//     try {
-//         const {id} = req.params;
-//         const product = await Product.findById(id);
-//         res.status(200).json(product);
-//     } catch (error) {
-//         res. status(500).json({message: error.message})
-//     }
-// })
-
-// app.post('/products', async(req, res) => {
-//     try {
-//         const product = await Product.create(req.body)
-//         res.status(200).json(product);
-    
-//     } catch (error) {
-//         console.log(error.message);
-//         res.status(500).json({message: error.message})
-//     }
-// })
-
-// // update a product
-// app.put('/products/:id', async(req, res) => {
-//     try {
-//         const {id} = req.params;
-//         const product = await Product.findByIdAndUpdate(id, req.body);
-//         // we cannot find any product in database
-//         if(!product){
-//             return res.status(404).json({message: `cannot find any product with ID ${id}`})
-//         }
-//         const updatedProduct = await Product.findById(id);
-//         res.status(200).json(updatedProduct);
-        
-//     } catch (error) {
-//         res. status(500).json({message: error.message})
-//     }
-// })
-
-// // delete a product
-// app.delete('/products/:id', async(req, res) =>{
-//     try {
-//         const {id} = req.params;
-//         const product = await Product.findByIdAndDelete(id);
-//         if(!product){
-//             return res.status(404).json({message: `cannot find any product with ID ${id}`})
-//         }
-//         res.status(200).json(product);
-
-//     } catch (error) {
-//         res. status(500).json({message: error.message})
-//     }
-// })
-
-// -------------------------------
 
 // create/add new task
 app.post('/tasks', async(req, res) => {
@@ -156,8 +94,8 @@ mongoose.
 connect('mongodb+srv://admin:admin123@alexdoesnotexistapi.dhlup.mongodb.net/Node-API?retryWrites=true&w=majority&appName=alexdoesnotexistAPI')
 .then(() => {
     console.log('connected to MongoDB')
-    app.listen(3000, ()=> {
-        console.log(`Node API app is running on port 3000`)
+    app.listen(5000, ()=> {
+        console.log(`Node API app is running on port 5000`)
     });
 }).catch((error) => {
     console.log(error)
